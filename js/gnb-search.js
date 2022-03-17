edfc0d3e2c08eec2e934fa3d1f39d30d6f11380c
devv-shinji <devv.shinji@gmail.com>
2022-03-17 14:03:50 +0900

    🚧 Implement deleting individual search-history-item

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -7,10 +7,16 @@ const deleteAllButton = gnbSearchHistory.querySelector(
   '.search-history-header button'
 )
 
-function closeGnbSearchHistory(e) {
+const deleteButtonList = gnbSearchHistoryList.querySelectorAll('.delete-button')
+
+function closeGnbSearchHistory() {
+  gnbSearchHistory.classList.remove('is-active')
+  window.removeEventListener('click', closeGnbSearchHistoryOnClickOutside)
+}
+
+function closeGnbSearchHistoryOnClickOutside(e) {
   if (!gnbSearch.contains(e.target)) {
-    gnbSearchHistory.classList.remove('is-active')
-    window.removeEventListener('click', closeGnbSearchHistory)
+    closeGnbSearchHistory()
   }
 }
 
@@ -21,7 +27,7 @@ function openGnbSearchHistory() {
 
   if (!gnbSearchHistory.classList.contains('is-active')) {
     gnbSearchHistory.classList.add('is-active')
-    window.addEventListener('click', closeGnbSearchHistory)
+    window.addEventListener('click', closeGnbSearchHistoryOnClickOutside)
   }
 }
 
@@ -29,41 +35,22 @@ gnbSearchInput.addEventListener('focus', openGnbSearchHistory)
 
 function deleteAllSearchHistoryItems() {
   gnbSearchHistoryList.innerHTML = ''
-  gnbSearchHistory.classList.remove('is-active')
+  closeGnbSearchHistory()
 }
 
 deleteAllButton.addEventListener('click', deleteAllSearchHistoryItems)
 
-// const gnbSearch = document.querySelector('.gnb-search')
-// const gnbSearchInput = gnbSearch.querySelector('input')
-// const gnbSearchHistory = gnbSearch.querySelector('.search-history')
-// const gnbSearchHistoryList = gnbSearch.querySelector('.search-history-list')
-// const gnbSearchHistoryItems = gnbSearch.querySelectorAll('.search-history-item')
-// const gnbSearchDeleteButtons = gnbSearch.querySelectorAll('.delete-button')
+function deleteSearchHistoryItem(e) {
+  e.stopPropagation()
 
-// function closeGnbSearchOnclickingOutside(e) {
-//   if (!gnbSearch.contains(e.target)) {
-//     gnbSearchHistory.classList.remove('is-active')
-//   }
-// }
+  const itemToDelete = this.parentNode
+  gnbSearchHistoryList.removeChild(itemToDelete)
 
-// function toggleGnbSearch() {
-//   gnbSearchHistory.classList.add('is-active')
+  if (gnbSearchHistoryList.children.length === 0) {
+    closeGnbSearchHistory()
+  }
+}
 
-//   if (gnbSearchHistory.classList.contains('is-active')) {
-//     window.addEventListener('click', closeGnbSearchOnclickingOutside)
-//   }
-// }
-
-// gnbSearchInput.addEventListener('focus', toggleGnbSearch)
-
-// gnbSearchDeleteButtons.forEach((button) => {
-//   button.addEventListener('click', function (e) {
-//     gnbSearchHistoryItems.forEach((item, index) => {
-//       if (item.contains(e.target)) {
-//         // newItems = Array.from(newItems).filter((item, idx) => idx !== index)
-//         gnbSearchHistoryItems[index].remove()
-//       }
-//     })
-//   })
-// })
+deleteButtonList.forEach((button) => {
+  button.addEventListener('click', deleteSearchHistoryItem)
+})
